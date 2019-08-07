0b0af4c850b8b6df24c13d5d3c1502baa464a53b
StaringPanda <47249205+StaringPanda@users.noreply.github.com>
2019-08-07 15:47:53 +0100

    Update Q1.cpp

--- cppDSmalik/ch2ex/ch2exercises/Q1.cpp
+++ cppDSmalik/ch2ex/ch2exercises/Q1.cpp
@@ -10,7 +10,7 @@ int main() {
     cout << "******************************************" << endl;
     cout << "*        Programming Assignment 1        *" << endl;
     cout << "*         Computer Programming I         *" << endl;
-    cout << "*          Author: Rob Stewart           *" << endl;
+    cout << "*          Author: user name             *" << endl;
     cout << "*       Due Date: Thursday, Jan. 24      *" << endl;
     cout << "******************************************";
     
